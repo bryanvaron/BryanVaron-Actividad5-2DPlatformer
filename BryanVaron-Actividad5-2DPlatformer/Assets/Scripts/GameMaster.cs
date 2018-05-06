b338c2ee081b2cd7b1f8db6d9f93449e8928412d
bryanvaron <bryanvaron@github>
2018-05-05 23:51:15 -0500

    Actividad 5
    Bryan Varon actividad 5

---
+++ BryanVaron-Actividad5-2DPlatformer/Assets/Scripts/GameMaster.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameMaster : MonoBehaviour {
+
+	public int coins;
+	public int hearts;
+	public Text coinsText;
+	public Text heartsText;
+
+	// Update is called once per frame
+	void Update () {
+
+		coinsText.text = ("Coins: " + coins);
+		heartsText.text = ("Hearts: " + hearts);
+	}
+}
